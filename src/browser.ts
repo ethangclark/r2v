d2eb95cc6ba9b4f98f8a2c4cbad22fbfadf01984
Ethan Gregory Clark <ethangclark@gmail.com>
2021-12-23 16:28:10 -0700

    improve browser dev commands

--- src/browser.ts
+++ src/browser.ts
@@ -2,9 +2,23 @@
  * This file is the entrypoint of browser builds.
  * The code executes when loaded in a browser.
  */
-import { foo } from './main'
+import { observable, autorun } from "./main";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(window as any).foo = foo  // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
+const obs1 = observable("obs1", {
+  a: 2,
+});
+const obs2 = observable("obs2", {
+  b: 3,
+});
 
-console.log('Method "foo" was added to the window object. You can try it yourself by just entering "await foo()"')
+autorun(() => {
+  console.log("a/b:", obs1.a, obs2.b);
+});
+
+obs1.setA(123);
+obs2.setB(1515);
+
+// // eslint-disable-next-line @typescript-eslint/no-explicit-any
+// (window as any).foo = foo  // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
+
+// console.log('Method "foo" was added to the window object. You can try it yourself by just entering "await foo()"')
